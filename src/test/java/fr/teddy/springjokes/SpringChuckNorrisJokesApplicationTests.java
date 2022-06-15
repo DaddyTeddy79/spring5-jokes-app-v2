5caa2a2597bacde4150570db04e2a2827ed31eef
DaddyTeddy79 <tcoudiere@yahoo.fr>
2022-06-15 21:08:00 +0200

    First Commit. Adding iniliazr files

---
+++ src/test/java/fr/teddy/springjokes/SpringChuckNorrisJokesApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.teddy.springjokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringChuckNorrisJokesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
